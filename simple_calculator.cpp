ef51310e70723aeb324b67317e6e9ced8ac532a7
Shashank Arya <shashankarya9831@gmail.com>
2024-04-02 07:25:41 +0530

    Create simple_calculator.cpp

---
+++ simple_calculator.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    double num1, num2;
+    char operation;
+  
+    cout << "Enter the First Number: ";
+    cin >> num1;
+    cout << "Enter the Second Number: ";
+    cin >> num2;
+
+    cout << "Choose an operation (+, -, *, /, %): ";
+    cin >> operation;
+
+    switch (operation)
+    {
+        case '+':
+            cout << "Result: " << ( num1 + num2 ) << endl;
+            break;
+      
+        case '-':
+            cout << "Result: " << ( num1 - num2 ) << endl;
+            break;
+      
+        case '*':
+            cout << "Result: " << ( num1 * num2 ) << endl;
+            break;
+      
+        case '/':
+            if( num2 == 0 ) 
+            {
+                cout << "Error: Division by zero is undefined" << endl;
+            } 
+            
+            else 
+            {
+                cout << "Result: " << (num1 / num2) << endl;
+            }
+            break;
+      
+        case '%':
+            if( num2 == 0 )
+            {
+                cout << "Error: Division by zero is undefined" << endl;  
+            }
+
+            else
+            {
+                cout << "Result: " << (num1 % num2) << endl;
+            }
+      
+        default:
+            cout << "Please choose a valid operation!" << endl;
+    }
+
+    return 0;
+}
